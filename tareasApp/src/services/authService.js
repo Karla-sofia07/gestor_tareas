d14a4e95604175cf238098fd709050400c31debc
Karla-sofia07 <124050136@upq.edu.mx>
2026-03-29 19:01:24 -0600

    DELETE

--- tareasApp/src/services/authService.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const login = (data) => api.post("/login", data);
-export const register = (data) => api.post("/register", data);
--- tareasApp/src/services/tareaService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getTareas = () => api.get("/tareas");
-export const createTarea = (data) => api.post("/tareas", data);
-export const updateTarea = (id, data) => api.put(`/tareas/${id}`, data);
-export const deleteTarea = (id) => api.delete(`/tareas/${id}`);
